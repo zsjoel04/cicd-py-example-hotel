30dd1646e3400098af74362f4bbb66bcb084149c
zsjoel04 <zsjoel111@gmail.com>
2026-07-08 17:30:50 +0200

    headless mode on

--- test/test_hootel.py
+++ test/test_hootel.py
@@ -10,6 +10,8 @@ class TestHootel(object):
     def setup_method(self):
         URL = 'http://hotel-v3.progmasters.hu/'
         options = Options()
+        options.add_argument('start-maximized')
+        options.add_argument("--headless")
         options.add_experimental_option("detach", True)
         self.browser = webdriver.Chrome(options=options)
         self.browser.get(URL)
